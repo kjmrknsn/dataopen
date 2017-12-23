6037e5e58ade3d7b0267b87297844ba5a188013f
Keiji Yoshida <kjmrknsn@gmail.com>
2017-12-23 16:38:34 +0900

    Update

---
+++ ddl/notebook.sql
@@ -0,0 +1,6 @@
+create table notebook (
+  id int unsigned not null auto_increment
+, created_at timestamp not null default current_timestamp
+, updated_at timestamp not null default current_timestamp on update current_timestamp
+, primary key (id)
+);
