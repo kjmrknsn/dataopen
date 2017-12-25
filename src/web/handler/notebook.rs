784cee501a297e72b88326726d0b13a4d1848090
Keiji Yoshida <kjmrknsn@gmail.com>
2017-12-25 12:15:39 +0900

    Update

--- src/web/handler/notebook.rs
+++ src/web/handler/notebook.rs
@@ -2,17 +2,26 @@ use iron::prelude::*;
 use iron::status;
 use persistent;
 use super::prelude::*;
-use super::super::super::notebook::Notebook;
 use super::super::mysql_pool::MysqlPool;
+use super::super::super::notebook::Notebook;
+use super::super::super::notebook_history::NotebookHistory;
 
 pub fn post(req: &mut Request) -> IronResult<Response> {
+    let uid = uid(&req);
+
     let mysql_pool =  req.get::<persistent::Read<MysqlPool>>().unwrap();
 
     let mut transaction = transaction(mysql_pool.as_ref())?;
 
     let notebook = result(Notebook::insert(
         &mut transaction,
-        &uid(&req)
+        &uid
+    ))?;
+
+    result(NotebookHistory::insert(
+        &mut transaction,
+        notebook.id,
+        &uid
     ))?;
 
     let notebook = json(&notebook)?;
